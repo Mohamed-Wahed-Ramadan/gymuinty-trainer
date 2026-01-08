a45bad9422b1d291a81203c45523da51750fb1e0
Mohamed Wahed <medo03459@gmail.com>
2026-01-08 09:49:45 +0200

    login and register

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -17,6 +17,8 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit, OnDestroy {
+  signupDropdownOpen = false;
+  signinDropdownOpen = false;
   currentLanguage = 'en';
   unreadMessagesCount = 0;
   isLoggedIn = false;
@@ -52,6 +54,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
         this.isLoggedIn = !!user && !!(user as any)?.userId;
         this.cdr.detectChanges();
       });
+    // Close dropdown on outside click
+    document.addEventListener('click', this.handleOutsideClick);
   }
 
   goToLogin(): void {
@@ -75,6 +79,50 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+    document.removeEventListener('click', this.handleOutsideClick);
+  }
+
+  toggleSignupDropdown(): void {
+    this.signupDropdownOpen = !this.signupDropdownOpen;
+  }
+
+  signupAsTrainer(): void {
+    this.signupDropdownOpen = false;
+    this.router.navigate(['/auth/login']);
+  }
+
+  signupAsClient(): void {
+    this.signupDropdownOpen = false;
+    window.open('https://gymunity-client.netlify.app', '_blank');
+  }
+
+  handleOutsideClick = (event: MouseEvent) => {
+    const signupDropdown = document.querySelector('.signup-dropdown-container');
+    const signinDropdown = document.querySelector('.signin-dropdown-container');
+    let changed = false;
+    if (signupDropdown && !signupDropdown.contains(event.target as Node)) {
+      if (this.signupDropdownOpen) changed = true;
+      this.signupDropdownOpen = false;
+    }
+    if (signinDropdown && !signinDropdown.contains(event.target as Node)) {
+      if (this.signinDropdownOpen) changed = true;
+      this.signinDropdownOpen = false;
+    }
+    if (changed) this.cdr.detectChanges();
+  };
+
+  toggleSigninDropdown(): void {
+    this.signinDropdownOpen = !this.signinDropdownOpen;
+  }
+
+  signinAsTrainer(): void {
+    this.signinDropdownOpen = false;
+    this.router.navigate(['/auth/login']);
+  }
+
+  signinAsClient(): void {
+    this.signinDropdownOpen = false;
+    window.open('https://gymunity-client.netlify.app', '_blank');
   }
 }
 
