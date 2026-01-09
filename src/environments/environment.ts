58b9f7d95ee64631bc7d909b4bdf3dfae5f54214
Mohamed Wahed <medo03459@gmail.com>
2026-01-09 17:44:02 +0200

    final update in shaa alaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   // Direct backend URL for local development
   //apiUrl: 'https://gymunity-fp-apis.runasp.net/api',
-  apiUrl: 'https://localhost:7209/api',
+  apiUrl: 'https://gymunity-fp-apis.runasp.net/api',
   googleClientId: '485010642516-dq1ietj5fe071h65v32ihdq3dfiir0ma.apps.googleusercontent.com', // Replace with your Google Client ID
 };
 
